9c66cdc201ddb8253090b788b3298af5d5c0a16f
RossWilliamsProject <94560328+RossWilliamsProject@users.noreply.github.com>
2022-03-30 16:35:39 +0100

    refactor(PrototypeC): changed styles

--- App.js
+++ App.js
@@ -151,7 +151,7 @@ export default function App() {
         <Stack.Screen name="CSAuthentication" component={CSAuthentication}
           options={{
             headerShown: true,
-            title: "Foudations",
+            title: "Authentication",
             headerStyle: { backgroundColor: "#189AB4" },
             headerTitleStyle: { fontWeight: "bold", color: "black" },
           }} />
